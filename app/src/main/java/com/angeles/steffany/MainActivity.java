10eced88b61a0b4920a0cbf43f9addbd8dd71118
SteffanyAngeles9 <2015081171@ust-ics.mygbiz.com>
2018-09-04 23:21:47 +0800

    Initial commit

---
+++ app/src/main/java/com/angeles/steffany/MainActivity.java
@@ -0,0 +1,17 @@
+package com.angeles.steffany;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint);
+        //setTitle("RelativeLayout");
+        //setTitle("LinearLayout");
+        setTitle("ConstraintLayout");
+
+    }
+}
